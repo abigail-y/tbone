a02e587e82dee3ca771673ee6467209fa77bd978
Abigail <ab841661@ucf.edu>
2026-04-30 00:54:47 -0400

    Update SCRIPT_URL to new Google Apps Script URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ─── Replace this with your deployed Google Apps Script URL ───────────────────
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxq0gw6F8NK1IY-DUfwP8sBz4kA3po90CmiWifMVr97mwSDCG0XMV9fPlGk3oFVbYJ45g/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyNmpN1cAVCcZkRn2n9Q8JjUaLZAXvTeFiiRQzxzJpw9-a5niHT7Q0SHwtiFgwz3jYRww/exec';
 
 
 const MONTH_NAMES = [
